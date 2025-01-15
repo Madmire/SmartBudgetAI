8f593f93835b779200a134bc1d810e0d2cee4d85
Madmire <andreadipama0@gmail.com>
2025-01-15 22:11:43 +0100

    Delete routes directory

--- routes/budget.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, render_template
-from models.user import User
-
-budget = Blueprint('budget', __name__)
-
-@budget.route('/update', methods=['POST'])
-def update_budget():
-    # Logique pour mettre à jour le budget et générer des statistiques
-    return render_template('dashboard.html', statistics=statistics)
--- routes/chatbot.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-chatbot = Blueprint('chatbot', __name__)
-
-@chatbot.route('/')
-def start():
-    return render_template('chatbot.html')
